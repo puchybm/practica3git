4e0e8cf67b90f4c77efa82d06b4fdb023bffa633
jorgehernandezfdez <jorge.lopesfernandez@gmail.com>
2020-03-02 17:25:31 +0100

    importacion del proyecto

---
+++ src/practica3git/Practica3git.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica3git;
+
+/**
+ *
+ * @author puchy
+ */
+public class Practica3git {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int edad = InputAsker.askInt("Cuantos años tienes?", 1, 120);
+        edad = calcularTiempoExistencia(edad);
+        System.out.println("Llevas vivo " + edad + " dias");
+
+    }
+
+    private static int calcularTiempoExistencia(int edad) {
+        return edad * 365;
+    }
+
+}
